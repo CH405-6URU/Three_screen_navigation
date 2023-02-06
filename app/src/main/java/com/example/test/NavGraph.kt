feb779bb12e0ff1183ebfe50ec88b961004cf964
cjgura <cjgura@gmail.com>
2023-02-05 20:34:51 -0500

    I got it!!

--- app/src/main/java/com/example/test/NavGraph.kt
+++ app/src/main/java/com/example/test/NavGraph.kt
@@ -1,4 +1,4 @@
-package com.example.practicenavigation3screens
+package com.example.test
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
@@ -6,6 +6,7 @@ import androidx.navigation.NavGraph
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.practicenavigation3screens.LoginScreen
 import com.example.test.DetailScreen
 import com.example.test.HomeScreen
 import com.example.test.Screens
